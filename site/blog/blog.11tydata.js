f3663005c2daf55251e9bc4fa762ad7dda5edd16
James-cloudcannon <james@cloudcannon.com>
2023-08-01 16:06:42 +1200

    creates blog 11ty data file

---
+++ site/blog/blog.11tydata.js
@@ -0,0 +1,33 @@
+const currentEnv = process.env.ELEVENTY_ENV || "development"
+const isDevEnv = currentEnv !== 'production';
+const todaysDate = new Date();
+
+function showDraft(data) {
+	const isDraft = 'draft' in data && data.draft !== false;
+	const isFutureDate = data.page.date > todaysDate;
+	return isDevEnv || (!isDraft && !isFutureDate);
+}
+
+module.exports = {
+	layout: "layouts/blog.liquid",
+	eleventyComputed: {
+		eleventyExcludeFromCollections: function(data) {
+			if (showDraft(data)) {
+				return data.eleventyExcludeFromCollections;
+			} else {
+				return true;
+			}
+		},
+		permalink: function(data) {
+			if (showDraft(data)) {
+				if (data.permalink) {
+					return data.permalink;
+				} else {
+					return "/blog/{{ title | slugify }}/";
+				}
+			} else {
+				return false;
+			}
+		}
+	}
+};
