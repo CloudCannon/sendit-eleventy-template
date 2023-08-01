fdfdbf76263c91331dbc44f105ad0c42915b4131
James-cloudcannon <james@cloudcannon.com>
2023-08-01 12:06:05 +1200

    feat: user can add dropdowns to navbar

--- site/js/script.js
+++ site/js/script.js
@@ -62,4 +62,17 @@ $(document).ready(function () {
     }
   });
   //end  Scroll spy style
+  $('.nav-item.dropdown > .dropdown-link').on('click', function(e) {
+    if($(window).width() < 991.98) {
+      e.preventDefault();
+      var dropdownOpened = $(this).parent().hasClass('show');
+      $('.dropdown').removeClass('show');
+      $('.dropdown-menu').removeClass('show');
+      
+      if (!dropdownOpened) {
+        $(this).next('.dropdown-menu').addClass('show');
+        $(this).parent('.dropdown').addClass('show');
+      }
+    }
+  });
 });
