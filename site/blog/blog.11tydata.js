3ef571db9c36adb86ff76d34592271076302671f
James-cloudcannon <james@cloudcannon.com>
2023-08-18 15:57:30 +1200

    updates showDraft to not need dev env

--- site/blog/blog.11tydata.js
+++ site/blog/blog.11tydata.js
@@ -1,5 +1,7 @@
-const currentEnv = process.env.ELEVENTY_ENV || "development"
-const isDevEnv = currentEnv !== 'production';
+// Set to true for development environment, false for production. 
+// When true, developers can see all posts (including drafts and future-dated posts) 
+// without having to manually change each post's front matter.
+const isDevEnv = false
 const todaysDate = new Date();
 
 function showDraft(data) {
@@ -9,7 +11,7 @@ function showDraft(data) {
 }
 
 module.exports = {
-	layout: "layouts/blog.liquid",
+	layout: "layouts/blog-single.liquid",
 	eleventyComputed: {
 		eleventyExcludeFromCollections: function(data) {
 			if (showDraft(data)) {
